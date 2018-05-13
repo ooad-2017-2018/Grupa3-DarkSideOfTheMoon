72162b14165ba31bc45e742371b15a8478af41ae
lamijaalagic <lalagic1@etf.unsa.ba>
2018-05-13 15:17:49 +0200

    oljg

--- Projekat/Dark Web/Model/Detektiv.cs
+++ Projekat/Dark Web/Model/Detektiv.cs
@@ -14,7 +14,7 @@ namespace Dark_Web.Model
         }
         public void napisiIzvjestaj()
         {
-            string izvjestaj = "pokupio iz textbox";
+            string izvjestaj = "pokupio iz textboxa";
             Izvjestaj noviIzvjestaj = new Izvjestaj(VrstaIzvjestaja.Hakiranje, izvjestaj); 
             GlobalnaMafija.mafija.DodajIzvjestaj(noviIzvjestaj); 
         }
